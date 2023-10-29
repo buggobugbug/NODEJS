18eecb96c50a28906e8ff637827f4b53e4c18424
PCM <pinsherlock1412@gmail.com>
2023-10-29 23:18:18 +0700

    Huy vua pull code len bai 18

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+
+//import express
 const app = express()
 const port = 8080
 
+//Khai bao routes
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! hoi Huy thu xem co  hoc nodejschua')
 })
 
 app.listen(port, () => {
