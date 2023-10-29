345057934f781ab6a0cd4967c30bdf27fa487b3d
PCM <pinsherlock1412@gmail.com>
2023-10-30 00:12:55 +0700

    Huy vua pull code len

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 //import express
 const app = express()
 const port = 8080
+const path = require('path')
+
+// config termplat engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 //Khai bao route
 
@@ -13,7 +18,8 @@ app.get('/', (req, res) => {
 //check duonbg link vidu nhu la http/8080/abc
 
 app.get('/abc', (req, res) => {
-    res.send('Check abc')
+    //res.send('Check abc')
+    res.render('sample.ejs')
 })
 
 app.get('/checkthehtml', (req, res) => {
@@ -23,3 +29,7 @@ app.get('/checkthehtml', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+// template engine (VIEW)
+
