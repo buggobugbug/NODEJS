953330b7596021c714e0b5e85d64b37c2d14682c
PCM <pinsherlock1412@gmail.com>
2023-10-29 23:27:28 +0700

    Huy vua pull bai 19 len

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-//Khai bao routes
+//Khai bao route
 
 app.get('/', (req, res) => {
     res.send('Hello World! hoi Huy thu xem co  hoc nodejschua')
+})
+
+//check duonbg link vidu nhu la http/8080/abc
+
+app.get('/abc', (req, res) => {
+    res.send('Check abc')
+})
+
+app.get('/checkthehtml', (req, res) => {
+    res.send('<h1>HTML OKE NHE</h1>')
 })
 
 app.listen(port, () => {
