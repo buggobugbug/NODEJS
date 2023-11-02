95fa23f132dbad863bcd48a0baf78c5825b82b7c
PCM <pinsherlock1412@gmail.com>
2023-11-02 23:26:58 +0700

    Huy vừa đẩy cơ sở dữ liệu, kết nối database vào

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const webRouter = require('./routes/web')
 const app = express()
 const port = process.env.PORT || 8080// hardcode
 const hostname = 'localhost'
+const mysql = require('mysql2')
 // config termplate engine
 
 configViewEngine(app);
@@ -18,6 +19,26 @@ configViewEngine(app);
 app.use('/v1', webRouter);
 app.use('/v1', webRouter);
 
+// TEST connection thử kết nối cơ sở dữ liệu
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, // truyền port chạy trên cổng 3307 thay vì chạy trên cổng 3306
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+});
+
+// kết nối từ nodejs chọc xuống database để lấy dữ liệu lên
+
+// simple query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(">>>>results= ", results); // results contains rows returned by server
+        console.log(">>>>fields= ",fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 app.listen(port, hostname, () => {
