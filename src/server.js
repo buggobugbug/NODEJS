9a8725ee94f7deda1ee6680b1f67cdd41942454d
PCM <pinsherlock1412@gmail.com>
2023-11-04 22:45:18 +0700

    hom nya đã fix được code

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const { Console } = require('console')
-const express = require('express')
-require('dotenv').config()
-console.log(">>>>check env", process.env)
+const { Console } = require('console');
+const express = require('express');
+require('dotenv').config();
+console.log(">>>>check env", process.env);
 const configViewEngine = require('./config/newengine');
-const webRouter = require('./routes/web')
+const webRouter = require('./routes/web');
+// const mysql = require('mysql2');
 //import express
-const app = express()
-const port = process.env.PORT || 8080// hardcode
-const hostname = 'localhost'
-const mysql = require('mysql2')
+const connection = require('./config/database')
+const app = express();
+const port = process.env.PORT || 8888;// hardcode
+const hostname = 'localhost';
+
 // config termplate engine
 
 configViewEngine(app);
@@ -17,26 +19,25 @@ configViewEngine(app);
 // Khai báo routes
 
 app.use('/v1', webRouter);
-app.use('/v1', webRouter);
 
-// TEST connection thử kết nối cơ sở dữ liệu
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, // truyền port chạy trên cổng 3307 thay vì chạy trên cổng 3306
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit'
-});
+
+// //test connection
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3307,
+//     user: 'root',
+//     password: '123456',
+//     database: 'hoidanit'
+// });
+
 
 // kết nối từ nodejs chọc xuống database để lấy dữ liệu lên
 
 // simple query
 connection.query(
-    'select * from Users u',
+    `select * from Users`,
     function (err, results, fields) {
         console.log(">>>>results= ", results); // results contains rows returned by server
-        console.log(">>>>fields= ",fields); // fields contains extra meta data about results, if available
     }
 );
 
@@ -46,4 +47,3 @@ app.listen(port, hostname, () => {
 })
 
 // template engine (VIEW)
-
